44eb64cdabb882daa9161ca8602f8fa34a341497
gufo <gufo.dev@gmail.com>
2026-05-08 07:12:44 +0200

    fix: load font with @fonts directive

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
 
+    @fonts
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @stack('head')
